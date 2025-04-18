ae23a17f39d19f55f69abaffbbb467a1d00ceabc
sh1xzyy <alekslungol@gmail.com>
2025-04-18 16:42:06 +0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor, store } from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
 	<Provider store={store}>
-		<StrictMode>
-			<App />
-		</StrictMode>
+		<PersistGate loading={null} persistor={persistor}>
+			<StrictMode>
+				<App />
+			</StrictMode>
+		</PersistGate>
 	</Provider>
 )
